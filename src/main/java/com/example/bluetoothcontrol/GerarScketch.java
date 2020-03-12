f09d9d1c3a0c9bc36cf4164bb2b471b251ffc3b8
pkaislan123 <comprasaislan@gmail.com>
2020-03-12 19:39:12 -0300

    refazendo tela para gerar scketch

--- src/main/java/com/example/bluetoothcontrol/GerarScketch.java
+++ src/main/java/com/example/bluetoothcontrol/GerarScketch.java
@@ -1,13 +1,33 @@
 package com.example.bluetoothcontrol;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.bluetoothcontrol.adapter.GerarSckecthAdapter;
+
 public class GerarScketch extends AppCompatActivity {
 
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    FragmentManager gerenciadorFragment = getSupportFragmentManager();
+
+
+
+    FragmentPanelSkecth fragmentPanelSkecth = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gerar_scketch);
+
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mViewPager = (ViewPager) findViewById(R.id.mViewPager);
+
+        mViewPager.setAdapter(new GerarSckecthAdapter(getSupportFragmentManager(), getResources().getStringArray(R.array.titles_tab)));
+        mTabLayout.setupWithViewPager(mViewPager);
+
+
     }
 }
---
+++ src/main/java/com/example/bluetoothcontrol/adapter/GerarSckecthAdapter.java
@@ -0,0 +1,51 @@
+package com.example.bluetoothcontrol.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.bluetoothcontrol.FragmentGerarSkecth;
+import com.example.bluetoothcontrol.FragmentPanelSkecth;
+
+public class GerarSckecthAdapter extends FragmentStatePagerAdapter {
+    private String[] mTabTitles;
+
+    public GerarSckecthAdapter(FragmentManager fm, String[]  mbTabTitles) {
+        super(fm);
+
+
+        this.mTabTitles = mbTabTitles;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch(i)
+        {
+            case 0:
+                return new FragmentPanelSkecth();
+            case 1:
+                return new FragmentGerarSkecth();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return this.mTabTitles.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int i)
+    {
+        return this.mTabTitles[i];
+    }
+
+
+
+
+
+}
+
+
